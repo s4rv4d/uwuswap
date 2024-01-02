e538699f82d214dadc2e7aae1b06ad83d83f468d
serverConnected <sarvadshetty@gmail.com>
2024-01-03 02:16:05 +0530

    set up liquidity and swap functionality for testing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import SwapBox from "./components/Swap-box/swapBox";
 import { MetaMaskProvider } from "./contexts/MetaMask";
-import MetaMask from "./components/Metamask";
+import EventsFeed from "./EventsFeed";
+
+const config = {
+  token0Address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  token1Address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  poolAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+  managerAddress: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+  ABIs: {
+    ERC20: require("./abi/ERC20.json"),
+    Pool: require("./abi/Pool.json"),
+    Manager: require("./abi/Manager.json"),
+  },
+};
 
 function App() {
   return (
     <MetaMaskProvider>
       <div className="App">
         <Navbar />
-        <SwapBox />
+        <SwapBox config={config} />
+        <footer>
+          <EventsFeed config={config} />
+        </footer>
       </div>
     </MetaMaskProvider>
   );
--- src/components/Swap-box/swapBox.js
+++ src/components/Swap-box/swapBox.js
@@ -1,16 +1,171 @@
 import React from "react";
 import { useState, useEffect, useContext } from "react";
-
 import "./swapBox.css";
+import { MetaMaskContext } from "../../contexts/MetaMask";
 
-export default function SwapBox() {
+const { ethers } = require("ethers");
+
+const tokenList = ["WETH", "USDC"];
+
+const TokensList = (props) => {
+  return (
+    <select defaultValue={props.selected} className="swap-dropdown">
+      {tokenList.map((t) => (
+        <option key={t}>{t}</option>
+      ))}
+    </select>
+  );
+};
+
+// add liquidity
+const addLiquidity = (
+  account,
+  { token0, token1, manager },
+  { managerAddress, poolAddress }
+) => {
+  console.log(token0);
+  // making sure token0 and token1 isnt null
+  if (!token0 || !token1) {
+    return;
+  }
+
+  // declare variables for test case
+  const amount0 = ethers.parseEther("0.998976618347425280"); // WETH
+  const amount1 = ethers.parseEther("5000"); // USDC
+  const lowerTick = 84222;
+  const upperTick = 86129;
+  const liquidity = ethers.BigNumber.from("1517882343751509868544");
+  const extra = ethers.defaultAbiCoder.encode(
+    ["address", "address", "address"],
+    [token0.address, token1.address, account]
+  );
+
+  console.log("here");
+
+  Promise.all([
+    token0.allowance(account, managerAddress),
+    token1.allowance(account, managerAddress),
+  ])
+    .then(([allowance0, allowance1]) => {
+      return Promise.resolve()
+        .then(() => {
+          if (allowance0.lt(amount0)) {
+            return token0
+              .approve(managerAddress, amount0)
+              .then((tx) => tx.wait());
+          }
+        })
+        .then(() => {
+          if (allowance1.lt(amount1)) {
+            return token1
+              .approve(managerAddress, amount1)
+              .then((tx) => tx.wait());
+          }
+        })
+        .then(() => {
+          return manager
+            .mint(poolAddress, lowerTick, upperTick, liquidity, extra)
+            .then((tx) => tx.wait());
+        })
+        .then(() => {
+          alert("Liquidity added!");
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("Failed to add liquidity");
+    });
+};
+
+// swap function - testing one way swap
+const swap = (
+  amountIn,
+  account,
+  { tokenIn, manager, token0, token1 },
+  { managerAddress, poolAddress }
+) => {
+  const amountInWei = ethers.utils.parseEther(amountIn);
+  const extra = ethers.utils.defaultAbiCoder.encode(
+    ["address", "address", "address"],
+    [token0.address, token1.address, account]
+  );
+
+  tokenIn
+    .allowance(account, managerAddress)
+    .then((allowance) => {
+      if (allowance.lt(amountInWei)) {
+        return tokenIn
+          .approve(managerAddress, amountInWei)
+          .then((tx) => tx.wait());
+      }
+    })
+    .then(() => {
+      return manager.swap(poolAddress, extra).then((tx) => tx.wait());
+    })
+    .then(() => {
+      alert("Swap succeeded!");
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("Failed!");
+    });
+};
+
+export default function SwapBox(props) {
+  const metamaskContext = useContext(MetaMaskContext);
+  const enabled = metamaskContext.status === "connected";
+
+  // just for testing
+  const amount0 = 0.008396714242162444;
+  const amount1 = 42;
+
   const [amount, setAmount] = useState("");
-  const [fromCurrency, setFromCurrency] = useState("ETH");
-  const [toCurrency, setToCurrency] = useState("DAI");
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [manager, setManager] = useState();
 
-  const handleSwap = () => {
-    console.log(`Swapping ${amount} ${fromCurrency} to ${toCurrency}`);
-    // Here you would add the logic to perform the swap, potentially using a blockchain API
+  useEffect(() => {
+    setFromCurrency(
+      new ethers.Contract(
+        props.config.token0Address,
+        props.config.ABIs.ERC20,
+        new ethers.BrowserProvider(window.ethereum).getSigner()
+      )
+    );
+    setToCurrency(
+      new ethers.Contract(
+        props.config.token1Address,
+        props.config.ABIs.ERC20,
+        new ethers.BrowserProvider(window.ethereum).getSigner()
+      )
+    );
+    setManager(
+      new ethers.Contract(
+        props.config.managerAddress,
+        props.config.ABIs.Manager,
+        new ethers.BrowserProvider(window.ethereum).getSigner()
+      )
+    );
+  }, []);
+
+  const addLiquidity_ = () => {
+    console.log(fromCurrency);
+
+    addLiquidity(
+      metamaskContext.account,
+      { fromCurrency, toCurrency, manager },
+      props.config
+    );
+  };
+
+  const swap_ = (e) => {
+    e.preventDefault();
+    swap(
+      amount1.toString(),
+      metamaskContext.account,
+      { tokenIn: fromCurrency, manager, fromCurrency, toCurrency },
+      props.config
+    );
   };
 
   return (
@@ -19,48 +174,39 @@ export default function SwapBox() {
         <div className="swap-header">
           <span>Swap</span>
           {/* <span>0.20% slippage</span> */}
+          <button disabled={!enabled} onClick={addLiquidity_}>
+            Add Liquidity
+          </button>
         </div>
+
+        {/* swap body */}
         <div className="swap-body">
           <div className="swap-input-container">
             <input
               type="number"
-              value={amount}
+              value={amount1}
               onChange={(e) => setAmount(e.target.value)}
               className="swap-input"
               placeholder="Enter an amount"
             />
-            <select
-              value={fromCurrency}
-              onChange={(e) => setFromCurrency(e.target.value)}
-              className="swap-dropdown"
-            >
-              <option>ETH</option>
-              <option>DAI</option>
-              {/* Add other currencies as needed */}
-            </select>
+
+            <TokensList selected="USDC" />
           </div>
 
           <div className="swap-input-container">
             <input
               type="number"
-              value={amount}
+              value={amount0}
               onChange={(e) => setAmount(e.target.value)}
               className="swap-input"
               placeholder="Enter an amount"
             />
-            <select
-              value={toCurrency}
-              onChange={(e) => setToCurrency(e.target.value)}
-              className="swap-dropdown"
-            >
-              <option>ETH</option>
-              <option>DAI</option>
-              {/* Add other currencies as needed */}
-            </select>
+
+            <TokensList selected="WETH" />
           </div>
 
-          {/* Repeat for second currency with toCurrency and setToCurrency */}
-          <button className="swap-action" onClick={handleSwap}>
+          {/* swap button */}
+          <button disabled={!enabled} className="swap-action" onClick={swap_}>
             Swap
           </button>
         </div>
@@ -68,3 +214,23 @@ export default function SwapBox() {
     </div>
   );
 }
+
+// {/* <select
+//               value={fromCurrency}
+//               onChange={(e) => setFromCurrency(e.target.value)}
+//               className="swap-dropdown"
+//             >
+//               <option>ETH</option>
+//               <option>DAI</option>
+//               {/* Add other currencies as needed */}
+//             </select> */}
+
+// {/* <select
+//               value={toCurrency}
+//               onChange={(e) => setToCurrency(e.target.value)}
+//               className="swap-dropdown"
+//             >
+//               <option>ETH</option>
+//               <option>DAI</option>
+//               {/* Add other currencies as needed */}
+//             </select> */}
